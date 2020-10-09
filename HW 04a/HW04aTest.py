82eb03afb96dfbd9aed35fced6ff3d306eddc20a
Olivia Powers <powersog@gmail.com>
2020-10-09 15:27:13 -0400

    using mocking for testing

--- HW 04a/HW04aTest.py
+++ HW 04a/HW04aTest.py
@@ -1,27 +1,47 @@
-import unittest
+import unittest, json
+from unittest.mock import Mock, patch
 import time
-from HW04a import GitHub
+import HW04a
+# from HW04a import GitHub
 
 class testGitHub(unittest.TestCase):
     time.sleep(1)
-    def test1pass(self):
-        self.assertEqual(GitHub('richkempinski'), ["Repo: csp Number of commits: 2", "Repo: hellogitworld Number of commits: 30",
+    @patch('HW04a.GitHub')
+    def test1pass(self, mockGitHubFunc):
+        mockGitHubFunc.return_value = ["Repo: csp Number of commits: 2", "Repo: hellogitworld Number of commits: 30",
+        "Repo: helloworld Number of commits: 6", "Repo: Mocks Number of commits: 10", "Repo: Project1 Number of commits: 2", 
+        "Repo: richkempinski.github.io Number of commits: 9", "Repo: threads-of-life Number of commits: 1", "Repo: try_nbdev Number of commits: 2", 
+        "Repo: try_nbdev2 Number of commits: 5"]
+        self.assertEqual(HW04a.GitHub('richkempinski'), ["Repo: csp Number of commits: 2", "Repo: hellogitworld Number of commits: 30",
         "Repo: helloworld Number of commits: 6", "Repo: Mocks Number of commits: 10", "Repo: Project1 Number of commits: 2", 
         "Repo: richkempinski.github.io Number of commits: 9", "Repo: threads-of-life Number of commits: 1", "Repo: try_nbdev Number of commits: 2", 
         "Repo: try_nbdev2 Number of commits: 5"])
+        # self.assertEqual(GitHub('richkempinski'), ["Repo: csp Number of commits: 2", "Repo: hellogitworld Number of commits: 30",
+        # "Repo: helloworld Number of commits: 6", "Repo: Mocks Number of commits: 10", "Repo: Project1 Number of commits: 2", 
+        # "Repo: richkempinski.github.io Number of commits: 9", "Repo: threads-of-life Number of commits: 1", "Repo: try_nbdev Number of commits: 2", 
+        # "Repo: try_nbdev2 Number of commits: 5"])
 
     time.sleep(1)
-    def test2pass(self):
-        self.assertEqual(GitHub('olivepowers'), ["Repo: Homework4 Number of commits: 30", "Repo: SSW345 Number of commits: 3", 
+    @patch('HW04a.GitHub')
+    def test2pass(self, mockGitHubFunc):
+        mockGitHubFunc.return_value = ["Repo: Homework4 Number of commits: 30", "Repo: SSW345 Number of commits: 3", 
+        "Repo: test-Homework4 Number of commits: 2"]
+        self.assertEqual(HW04a.GitHub('olivepowers'), ["Repo: Homework4 Number of commits: 30", "Repo: SSW345 Number of commits: 3", 
         "Repo: test-Homework4 Number of commits: 2"])
+        # self.assertEqual(GitHub('olivepowers'), ["Repo: Homework4 Number of commits: 30", "Repo: SSW345 Number of commits: 3", 
+        # "Repo: test-Homework4 Number of commits: 2"])
 
     time.sleep(1)
-    def test3fail(self):
-        self.assertEqual(GitHub('abcdeefghijklmnopqrstuvwxyz'), "This account is not valid")
+    @patch('HW04a.GitHub')
+    def test3fail(self, mockGitHubFunc):
+        mockGitHubFunc.return_value = "This account is not valid"
+        self.assertEqual(HW04a.GitHub('abcdeefghijklmnopqrstuvwxyz'), "This account is not valid")
 
     time.sleep(1)
-    def test4fail(self):
-        self.assertEqual(GitHub(19283894389), "This account is not valid")
+    @patch('HW04a.GitHub')
+    def test4fail(self, mockGitHubFunc):
+        mockGitHubFunc.return_value = "This account is not valid"
+        self.assertEqual(HW04a.GitHub(19283894389), "This account is not valid")
 
 
 
